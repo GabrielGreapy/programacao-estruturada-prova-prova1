596e2621ed74852707d60112b813297578b483b2
GabrielGreapy <159476031+GabrielGreapy@users.noreply.github.com>
2024-07-12 19:37:09 +0000

    prvinha dlc

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 def q1():
-    pass
+    contador = 0
+    numero = int(input(""))
+    for i in range(1, numero + 1):
+        if (numero % i == 0) and (i % 3 == 0):
+                contador += 1
+    if contador == 0:
+        print(f'O número não possui divisores multiplos de 3')
+    else:
+        print(f'Quantidade de divisores divisiveis por 3:', contador)
 
 def q2():
-    pass
+    menor = int(input(""))
+    maior = int(input(""))
+    soma = 0
+    if menor > maior:
+        n1 = menor
+        menor = maior
+        maior = n1
+    for i in range(menor, maior + 1):
+        if i > 0 :
+            soma += i
+    print(soma)
+
 
 def q3():
+    
     pass
 
 def q4():
